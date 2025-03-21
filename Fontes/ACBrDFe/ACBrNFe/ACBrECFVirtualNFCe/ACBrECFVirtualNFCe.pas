66f9a095fad74802ac5f476eaf60063a8f3dccde
Davi Souza <davi@highsoftsistemas.com.br>
2025-03-21 16:44:36 -0300

    Ref. OS: 23254 Cliente: HIGHSOFT SISTEMAS LTDA ME Obs: Ajustado NFCe no frente de caixa quando concedido desconto a nível de nota não estava reduzindo da base de ICMS do item.

--- Fontes/ACBrDFe/ACBrNFe/ACBrECFVirtualNFCe/ACBrECFVirtualNFCe.pas
+++ Fontes/ACBrDFe/ACBrNFe/ACBrECFVirtualNFCe/ACBrECFVirtualNFCe.pas
@@ -752,11 +752,23 @@ begin
           if NotasFiscais.Items[0].NFe.Det[i].Prod.cProd <> cItemCancelado then
           begin
             if fpCupom.DescAcresSubtotal > 0 then
-              NotasFiscais.Items[0].NFe.Det[i].Prod.vOutro :=
-                NotasFiscais.Items[0].NFe.Det[i].Prod.vOutro + ItDescAcre[i]
+            begin
+              NotasFiscais.Items[0].NFe.Det[i].Prod.vOutro := NotasFiscais.Items[0].NFe.Det[i].Prod.vOutro + ItDescAcre[i];
+              if NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC > 0 then
+              begin
+                NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC   := NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC + ItDescAcre[i];
+                NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vICMS := NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC * (NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.pICMS/100);
+              end;
+            end
             else
-              NotasFiscais.Items[0].NFe.Det[i].Prod.vDesc :=
-                NotasFiscais.Items[0].NFe.Det[i].Prod.vDesc + ItDescAcre[i];
+            begin
+              NotasFiscais.Items[0].NFe.Det[i].Prod.vDesc := NotasFiscais.Items[0].NFe.Det[i].Prod.vDesc + ItDescAcre[i];
+              if NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC > 0 then
+              begin
+                NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC   := NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC - ItDescAcre[i];
+                NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vICMS := NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.vBC * (NotasFiscais.Items[0].NFe.Det[i].Imposto.ICMS.pICMS/100);
+              end;
+            end;
           end;
         end;
       end;
