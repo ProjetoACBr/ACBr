be91a8b64a8f67a749fdc4d02883458f2e0908af
renatorubinho <renatorubinho@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-08-28 20:37:02 +0000

    28/08/2025 -- pcesConversaoeSocial -- [+] #ACBR-7979: Novos itens no enumerado tpMotivosAfastamento - https://www.projetoacbr.com.br/forum/topic/85248-atualiza%C3%A7%C3%A3o-dos-c%C3%B3digos-de-motivos-de-afastamento-do-evento-s-2230/   Por: Sófolha
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@41154 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesConversaoeSocial.pas
+++ Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesConversaoeSocial.pas
@@ -598,7 +598,10 @@ type
                               mtvSuspensaoPagamentoServidorNaoRecadastrado,               // 39
                               mtvExercicioOutroOrgaoEmpregadoCedido,                      // 40
                               mtvQualificacaoAfastamentoSuspencaoArt17_MP1116_2022,       // 41
-                              mtvQualificacaoAfastamentoSuspencaoArt19_MP1116_2022);      // 42
+                              mtvQualificacaoAfastamentoSuspencaoArt19_MP1116_2022,       // 42
+                              mtvLicencaMaternidadeProrrogacao60DiasLei15156_2025,        // 43
+                              mtvSuspensaoContratualAjuizamentoRescisaoIndireta,          // 44
+                              mtvSuspensaoContratualAjuizamentoInqueritoFaltaGrave);      // 45
 
   tpTpAcidTransito        = (tpatAtropelamento, tpatColisao, tpatOutros, tpatNao);
 
@@ -1360,14 +1363,15 @@ const
 
   TMotivoAlteracaoCargoFuncao: array[0..3] of string = ('1', '2', '3', '9');
 
-  TMotivoAfastamento: array[1..38] of string = ('01', '03', '05', '06', '07',
+  TMotivoAfastamento: array[1..41] of string = ('01', '03', '05', '06', '07',
                                                 '08', '10', '11', '12', '13',
                                                 '14', '15', '16', '17', '18',
                                                 '19', '20', '21', '22', '23',
                                                 '24', '25', '26', '27', '28',
                                                 '29', '30', '31', '33', '34',
                                                 '35', '36', '37', '38', '39',
-                                                '40', '41', '42');
+                                                '40', '41', '42', '43', '44',
+                                                '45');
 
   TGenericosString0_1 : array[0..1] of string = ('0','1' );
   TGenericosString0_2 : array[0..2] of string = ('0','1','2' );
