f727318590e97cd1c3f98c390844f4f726458b50
eliias <eliias@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-09-16 14:50:24 +0000

    - Correção do commit anterior.
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@41538 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTEFD/ACBrTEFSmartTEFAPI.pas
+++ Fontes/ACBrTEFD/ACBrTEFSmartTEFAPI.pas
@@ -1,4 +1,4 @@
-ï»¿{******************************************************************************}
+{******************************************************************************}
 { Projeto: Componentes ACBr                                                    }
 {  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
 { mentos de Automação Comercial utilizados no Brasil                           }
