95fb7e10ce84ba18e74f5988d615e311da5eeb08
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-09-17 22:53:14 +0000

    -- Demo NFe Go -- [+] Adiciona exemplo de alocação de buffer (simples)   Por: doszapro
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@41561 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/Go/NFe/nfe.go
+++ Projetos/ACBrLib/Demos/Go/NFe/nfe.go
@@ -12,14 +12,27 @@ import (
 	"fmt"
 	"unsafe"
 )
-
 func main (){
 	var handle  unsafe.Pointer = nil;
+	size := C.int(100);
+	// aloca buffer
+	s := make([]byte, size);
+	// ponteiro char * do s
+	buffer := (*C.char) (unsafe.Pointer(&s[0])) ;
+
+
 	configFile := C.CString("./acbrlib.ini");
 	chaveCrypt := C.CString("");
 
-	result:= C.NFE_Inicializar(&handle, configFile,chaveCrypt);
+	result := C.NFE_Inicializar(&handle, configFile,chaveCrypt);
 	fmt.Println("Result:", result);
+
+	// exemplo de uso da funcao NFE_Nome
+	// para servir de exemplo de uso do buffer alocado e do size
+	
+	result = C.NFE_Nome(handle,  buffer, &size);
+	fmt.Println("Result:", result, "Size:", size);
+	fmt.Println("Name:", string(s));
 
 	result = C.NFE_Finalizar(handle);
 	fmt.Println("Result:", result);
