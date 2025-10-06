afe0cff6283d36cfb42176d9f44dffc0a6e91e43
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-10-06 12:24:38 +0000

    [+]ACBR-8156 Duplicidade do Exportador PDF proposto por Marcos R Weimer / compatibilizado por BigWings
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@41926 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrComum/ACBrUtil.FR.pas
+++ Fontes/ACBrComum/ACBrUtil.FR.pas
@@ -76,4 +76,28 @@ begin
 end;
 {$ENDIF}
 
+procedure RemoveExportPDFDup;
+var
+  LCount, I: Integer;
+begin
+  //Remove do menu, exportações de PDF "duplicadas"
+  //FastReport varre a aplicação por RTTI buscando TfrxPDFExport
+  //Para cada TfrxPDFExport é criado um item no menu
+  //Este processo varre os plugins de exportação deixando apenas 1 (o ultimo) TfrxPDFExport por rtti
+  //Inserir na chamada do metodo Imprimir do Relatório
+  //proposto por Marcos R Weimer / compatibilizado por BigWings
+  LCount := 0;
+
+  for i := Pred(frxExportFilters.Count) downto 0 do
+  begin
+    if AnsiUpperCase(frxExportFilters[i].Filter.ClassName) = 'TFRXPDFEXPORT' then
+    begin
+      if LCount > 0 then
+        frxExportFilters.Delete(i)
+      else
+        Inc(LCount);
+    end;
+  end;
+end;
+
 end.
