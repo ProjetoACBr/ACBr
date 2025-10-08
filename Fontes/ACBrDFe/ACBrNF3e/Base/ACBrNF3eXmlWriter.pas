abe488abb77a41ec80fb32825972abe178cc3850
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-10-08 16:54:13 +0000

    08/10/2025 -- ACBrNF3eXmlWriter -- [*] Ajustes para gerar o grupo gIBSCBS em função dos CST permitidos.    Por: Italo Giurizzato Junior
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@42064 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNF3e/Base/ACBrNF3eXmlWriter.pas
+++ Fontes/ACBrDFe/ACBrNF3e/Base/ACBrNF3eXmlWriter.pas
@@ -2288,7 +2288,7 @@ begin
     Result.AppendChild(AddNode(tcStr, '#3', 'indDoacao', 1, 1, 0,
               pcnConversao.TIndicadorExToStr(IBSCBS.indDoacao), DSC_INDDOACAO));
 
-    if IBSCBS.gIBSCBS.vBC > 0 then
+    if IBSCBS.CST in [cst000, cst510, cst830] then
       Result.AppendChild(Gerar_IBSCBS_gIBSCBS(IBSCBS.gIBSCBS));
   end;
 end;
