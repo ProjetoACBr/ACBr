706b0b6f82e7a3e971f888ed4e3daf9cabfe37c5
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-10-09 23:02:27 +0000

    09/10/2025 [*] Atualização dos pacotes de instalação Delphi e Lazarus. Por: Italo Giurizzato Junior
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@42126 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBrDFe/ACBrMDFe/ACBr_MDFe.pas
+++ Pacotes/Lazarus/ACBrDFe/ACBrMDFe/ACBr_MDFe.pas
@@ -9,12 +9,13 @@ interface
 
 uses
   ACBrMDFe, ACBrMDFeConfiguracoes, ACBrMDFeManifestos, ACBrMDFeReg, 
-  ACBrMDFeWebServices, pmdfeConversaoMDFe, pmdfeMDFeR, pmdfeMDFeW, 
+  ACBrMDFeWebServices, pmdfeConversaoMDFe, 
   ACBrMDFeDAMDFeClass, ACBrMDFe.Classes, ACBrMDFe.ProcInfraSA, 
   ACBrMDFe.XmlReader, ACBrMDFe.XmlWriter, ACBrMDFe.ConsNaoEnc, 
   ACBrMDFe.RetConsNaoEnc, ACBrMDFe.ConsSit, ACBrMDFe.RetConsSit, 
   ACBrMDFe.EventoClass, ACBrMDFe.EnvEvento, ACBrMDFe.RetEnvEvento, 
-  ACBrMDFe.Consts, ACBrMDFe.IniReader, ACBrMDFe.IniWriter, LazarusPackageIntf;
+  ACBrMDFe.Consts, ACBrMDFe.IniReader, ACBrMDFe.IniWriter, 
+  ACBrMDFe.ValidarRegrasdeNegocio, ACBrMDFe.Conversao, LazarusPackageIntf;
 
 implementation
 
