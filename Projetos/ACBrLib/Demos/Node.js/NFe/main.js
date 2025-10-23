0eba29fd81a665d4904c00dbc1f1bed614cfb489
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-10-23 14:58:02 +0000

    -- Demo ACBrLibNFe Nodejs --
    [+] Adiciona informaçoes sobre configuração do TipoDANFe e ModeloDF, ambas informações auxiliaram o usuário para configurar corretamente a emissão de NFe ou NFCe
    [-] Chama o finalizar dentro de finally, previnindo vazamentos de memória.
    
     Por: doszapro
    
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@42431 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/Node.js/NFe/main.js
+++ Projetos/ACBrLib/Demos/Node.js/NFe/main.js
@@ -43,9 +43,16 @@ const ACBrLibNFeMT = require('@projetoacbr/acbrlib-nfe-node/dist/src').default;
 // a dll ou so devem estar na pasta lib
 // a acbrlib (dll ou so) é carrega de acordo com o sistema operacional
 // para windows usar a versao 64 bits MT CDECL
+// outra observação importante para windows: colocar as dlls de dependências (libxml2, openssl) na mesma pasta  do executável do node 
 // para Linux usar a versão console MT 64 bits
 
 // docker: recomendado usar a versão ubuntu:noble (testado com sucesso)
+
+//obsevações importantes para usuários linux:
+// instalar o pacote nodejs oficial do ubuntu (ou debian) ue npm
+// podem ocorrer falhas de segmentação se usar o pacote do repositório oficial do nodejs
+// isso porque o nodejs oficial é compilado com openssl estático e pode entrar em conflito com a lib openssl usada pela ACBrLibNFe
+// sudo apt install nodejs npm
 
 // lembre-se de criar as pastas:
 //  lib
@@ -81,6 +88,21 @@ let acbrNFe = new ACBrLibNFeMT(pathACBrLibNFe, eArqConfig, eChaveCrypt);
 function configuraSessaoDANFE() {
   acbrNFe.configGravarValor("DANFE", "PathPDF", path.resolve(__dirname, 'data', 'pdf'))
 
+  //configura o tipo de DANFE
+
+
+  //0 = tiSemGeracao
+  //1 = tiRetrato
+  //2 = tiPaisagem
+  //3 = tiSimplificado
+  //4 = tiNFCe
+  //5 = tiMsgEletronica
+
+  // lembre-se de descomentar a linha abaixo para definir o tipo de DANFE desejado
+
+  // por exemplo, para NFCe, use : acbrNFe.configGravarValor("DANFE", "TipoDANFE", "4") 
+  // acbrNFe.configGravarValor("DANFE", "TipoDANFE", "0")
+
 }
 
 /**
@@ -95,6 +117,12 @@ function configuraSessaoNFe() {
   //seta ambiente de homologação
   acbrNFe.configGravarValor("NFE", "Ambiente", "1")
 
+  //seta o modelo da NFe
+  // O é NFe
+  // 1 é NFCe
+  // em breve será disponibiizado um enumerado para esses valores
+  //acbrNFe.configGravarValor("NFE", "ModeloDF", "0")
+
 }
 
 
@@ -105,8 +133,8 @@ function configuraSessaoNFe() {
 function configuraSessaoDFe() {
 
   //tenta ler a senha do pfx a partir da var de ambiente PFX_PASSWORD
-  // apenas para teste
-  //não fazer isso em produção
+  // PFX_PASSWORD deve ser definida no arquivo .env na raiz do projeto
+  // evite deixar a senha hardcoded no código fonte
 
   let senhaPFX = process.env.PFX_PASSWORD;
 
@@ -166,11 +194,26 @@ try {
   acbrNFe.obterXml(0)
 
   // acbrNFe.enviar()
-  console.log(acbrNFe.statusServico())
-  console.log(acbrNFe.openSslInfo())
 
-  acbrNFe.finalizar()
+  //evite consultar status antes de enviar alguma nota
+  // isso pode causar rejeição por consumo indevido do serviço
+
+  console.log(acbrNFe.statusServico())
+  //console.log(acbrNFe.openSslInfo())
 
 } catch (error) {
   console.error(error)
+} finally {
+  // Finaliza a biblioteca
+  //liberar os recursos alocados pela biblioteca
+  if (acbrNFe) {
+
+    acbrNFe.finalizar()
+    //obsevação para typescript: ao declarar a variavel com using o dispose é chamado automaticamente
+    // nesse caso o symbol.dispose() não é necessário
+    // o Symbol.dispose]() chama o método finalizar internamente
+   //acbrNFe[Symbol.dispose]()
+    acbrNFe = null
+  }
+
 }
