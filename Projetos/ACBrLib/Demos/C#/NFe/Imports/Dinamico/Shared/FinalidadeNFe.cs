59274efa4bf96af6a1bbd460b05e2291f2a3ee5f
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-11-17 21:18:06 +0000

    -- ACBrLib NFe C# -- [+] Adiciona FinalidadeNFe.cs  #ACBR-8319
    Por: SET SYSTEM INFORMÁTICA
    
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@42943 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/NFe/Imports/Dinamico/Shared/FinalidadeNFe.cs
@@ -0,0 +1,12 @@
+﻿namespace ACBrLib.NFe
+{
+    public enum FinalidadeNFe
+    {
+        fnNormal = 1,
+        fnComplementar = 2,
+        fnAjuste = 3,
+        fnDevolucao = 4,       
+        fnCredito = 5,
+        fnDebito = 6,
+    }
+}
