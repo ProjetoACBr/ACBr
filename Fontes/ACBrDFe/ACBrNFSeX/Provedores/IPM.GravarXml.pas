8e289acf51ca2285cc36b76fa16b8eb2957a6c1f
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-12-02 13:38:01 +0000

    02/12/2025 -- IPM.GravarXml -- [*] ACBR-8510 - Ajustes por conta da alteração nos schemas disponibilizado pelo provedor.    Por: marciost
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@43384 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/IPM.GravarXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/IPM.GravarXml.pas
@@ -228,7 +228,6 @@ begin
   FpNrOcorrCodigoAtividade := -1;
 
   // Reforma Tributária
-  NrOcorrtpOper := -1;
   NrOcorrindDest := -1;
   GerarDest := False;
   GerargReeRepRes := False;
@@ -813,12 +812,15 @@ begin
       begin
         GerarTagNifTomador := True;
         GerarEnderecoExterior := True;
+
         NrOcorrCodigoMunicInterm := 1;
         NrOcorrDiscriminacao_1 := -1;
         NrOcorrCodigoMunic_1 := -1;
-        NrOcorrCodigoNBS := 1;
-        NrOcorrDiscriminacao_2 := 1;
-        NrOcorrCodigoMunic_2 := 1;
+
+        NrOcorrCodigoNBS := 0;
+        NrOcorrCodTribMun_2 := 0;
+        NrOcorrDiscriminacao_2 := 0;
+        NrOcorrCodigoMunic_2 := 0;
       end;
   else
     begin
