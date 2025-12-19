29ddc49339099450011f988f0854e852ec3d497d
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-12-19 17:04:31 +0000

    19/12/2025 -- ACBrNFSeXConversao/ACBrNFSeXProviderBase -- [*] ACBr-8614 Incluído a Natureza da Operação 305.    Por: Gustavo Totta
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@43718 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Base/Provedores/ACBrNFSeXProviderBase.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Base/Provedores/ACBrNFSeXProviderBase.pas
@@ -1746,6 +1746,7 @@ begin
     no121 : Result := '121 - ISS Fixo (Sociedade de Profissionais)';
     no201 : Result := '201 - ISS retido pelo tomador ou intermediário do serviço';
     no301 : Result := '301 - Operação imune, isenta ou não tributada';
+    no305 : Result := '305 - Imunidade Tributária';
     no501 : Result := '501 - ISS devido no município (Simples Nacional)';
     no511 : Result := '511 - Prestação de serviço no município - iss mensal sem retenção na fonte';
     no512 : Result := '512 - Prestação de serviço no município - iss mensal com retenção na fonte';
