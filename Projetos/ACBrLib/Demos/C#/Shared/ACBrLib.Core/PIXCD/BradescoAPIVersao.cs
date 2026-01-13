a47efa2737ef1d66b7fe1c15131fff3e73f89d87
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-01-13 19:32:59 +0000

    Inclusão Classe BradescoAPIVersao.cs
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@44257 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/PIXCD/BradescoAPIVersao.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ACBrLib.Core.PIXCD
+{
+    public enum BradescoAPIVersao
+    {
+        [Description("API Versão 1")]
+        braVersao1 = 0,
+
+        [Description("API Versão 2")]
+        braVersao2 = 1,
+    }
+}
