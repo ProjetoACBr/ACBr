92efffc8227e15845b0fcd8510c063abb3739ad0
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-01-23 12:48:10 +0000

    23/01/2026 -- Demo ACBrLibPIXCD Android -- [-] Refatoraç código removendo Activities desnecessárias (Refatoração PIXCD parte I)
     Por: doszapro
    
    
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@44403 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/Android/Java/PIXCD/app/src/main/java/com/acbr/pixcd/acbrlibpixcd/demo/comandos/MainActivity.java
+++ Projetos/ACBrLib/Demos/Android/Java/PIXCD/app/src/main/java/com/acbr/pixcd/acbrlibpixcd/demo/comandos/MainActivity.java
@@ -2,6 +2,7 @@ package com.acbr.pixcd.acbrlibpixcd.demo.comandos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,12 @@ import com.acbr.pixcd.acbrlibpixcd.demo.R;
 import com.acbr.pixcd.acbrlibpixcd.demo.comandos.endpoints.cob.ComandosEndPointCobActivity;
 import com.acbr.pixcd.acbrlibpixcd.demo.comandos.endpoints.cobv.ComandosEndPointCobVActivity;
 import com.acbr.pixcd.acbrlibpixcd.demo.comandos.endpoints.pix.ComandosEndPointPixActivity;
+import com.acbr.pixcd.acbrlibpixcd.demo.configuracoes.ConfiguracoesPIXCDActivity;
 import com.acbr.pixcd.acbrlibpixcd.demo.utils.PIXCDApplication;
 
 import br.com.acbr.lib.pixcd.ACBrLibPIXCD;
 
-public class ComandosPIXCDActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private ACBrLibPIXCD ACBrPIXCD;
     private PIXCDApplication application;
@@ -28,14 +30,17 @@ public class ComandosPIXCDActivity extends AppCompatActivity {
         Button btnEndPointPix = findViewById(R.id.btnEndPointPix);
         Button btnEndPointCob = findViewById(R.id.btnEndPointCob);
         Button btnEndPointCobV = findViewById(R.id.btnEndPointCobV);
+        Button btnConfiguracoes = findViewById(R.id.btnAbrirConfiguracoes);
 
         btnQRCodeEstatico.setOnClickListener(view -> IrParaTelaQRCodeEstatico());
         btnEndPointPix.setOnClickListener(view -> IrParaTelaEndPointPix());
         btnEndPointCob.setOnClickListener(view -> IrParaTelaEndPointCob());
         btnEndPointCobV.setOnClickListener(view -> IrParaTelaEndPointCobV());
+        btnConfiguracoes.setOnClickListener(view -> IrParaTelaConfiguracoes());
 
         this.application = (PIXCDApplication) getApplicationContext();
         this.ACBrPIXCD = application.getACBrLibPIXCD();
+        this.configurarACBrPIXCD();
     }
 
     private void IrParaTelaQRCodeEstatico(){
@@ -56,5 +61,36 @@ public class ComandosPIXCDActivity extends AppCompatActivity {
     private void IrParaTelaEndPointCobV(){
         Intent intent = new Intent(this, ComandosEndPointCobVActivity.class);
         startActivity(intent);
+    }
+
+    private void IrParaTelaConfiguracoes(){
+        Intent intent = new Intent(this, ConfiguracoesPIXCDActivity.class);
+        startActivity(intent);
+    }
+
+    private void configurarACBrPIXCD() {
+        try {
+            ACBrPIXCD.inicializar();
+            aplicarConfiguracoesPadrao();
+        } catch (Exception e) {
+            Log.e("ACBrLibPIXCD", "Erro ao inicializar ACBrLibPIXCD", e);
+        }
+    }
+
+    private void aplicarConfiguracoesPadrao(){
+        try{
+            ACBrPIXCD.configGravarValor("Principal", "LogPath", application.getLogPath());
+            ACBrPIXCD.configGravarValor("Principal", "LogNivel", "4");
+            ACBrPIXCD.configGravar();
+            ACBrPIXCD.configLer(application.getArqConfigPath());
+        } catch (Exception e){
+            Log.e("MainActivity", e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        ACBrPIXCD.finalizar();
     }
 }
